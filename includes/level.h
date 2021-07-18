3db8fc7a947abe02f97a127daa5d9ec6f86b0353
leadpogrommer <ilyamerzlakov@gmail.com>
2021-07-19 00:22:53 +0700

    fix build on my machine (probably gcc 11)

--- includes/level.h
+++ includes/level.h
@@ -22,12 +22,12 @@ void map_rooms_to_chars(level_t* level);
 void init_level_char_grid(level_t* level);
 level_t* get_level_populated_with_rooms(int room_count, room_pool_t* room_pool);
 level_t* get_level_with_room_grid(int room_count, room_pool_t* room_pool);
-level_t* cropped_level(int map_size, int room_count,  room_t *room_grid[room_count][room_count], vector2_pair_array_t* connected_rooms);
+level_t* cropped_level(int map_size, int room_count,  room_t *room_grid[map_size][map_size], vector2_pair_array_t* connected_rooms);
 void correct_connected_rooms_grid_pos(level_t* level, vector2_t left_up_corner);
 void set_room_grid_cell_sizes(level_t* level);
 void connect_doors(level_t* level);
 void shut_all_open_doors(level_t* level);
-int room_placer(int rooms_left, int* branches_left, room_pool_t* room_pool, int room_count, room_t* rooms[room_count * 2 + 1][room_count * 2 + 1], vector2_t previous_direction, vector2_t pos, vector2_pair_array_t* connected_rooms);
+int room_placer(int rooms_left, int* branches_left, room_pool_t* room_pool, int room_count, int array_size, room_t* rooms[array_size][array_size], vector2_t previous_direction, vector2_t pos, vector2_pair_array_t* connected_rooms);
 
 void log_level_to_file(level_t* level, const char* filename);
 
